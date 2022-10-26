308533af97838062cf4ed4586bb6716bfb74bee0
Alisa Ivanenko <aiva@Alisas-MacBook-Pro.local>
2022-10-26 13:18:11 +0300

    Add basic DiscountCalculator class

---
+++ src/main/java/kata/supermarket/DiscountCalculator.java
@@ -0,0 +1,10 @@
+package kata.supermarket;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class DiscountCalculator {
+    public BigDecimal calculateDiscount(List<ItemByUnit> items) {
+        return BigDecimal.ZERO;
+    }
+}
---
+++ src/test/java/kata/supermarket/DiscountCalculatorTest.java
@@ -0,0 +1,30 @@
+package kata.supermarket;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DiscountCalculatorTest {
+    private DiscountCalculator calculator;
+
+    @BeforeEach
+    public void setup() {
+        calculator = new DiscountCalculator();
+    }
+
+    @Test
+    public void returnsZero_whenNoItems() {
+        assertEquals(BigDecimal.ZERO, calculator.calculateDiscount(List.of()));
+    }
+
+    @Test
+    public void returnZero_whenSingleItem() {
+        BigDecimal price = BigDecimal.valueOf(1.49);
+        List<ItemByUnit> item = List.of(new ItemByUnit(new Product(price)));
+        assertEquals(BigDecimal.ZERO, calculator.calculateDiscount(item));
+    }
+}
